29cb4d5b4e179dedeca2b48d526df703dde14c64
Christian Zavala <cezavala@espol.edu.ec>
2025-06-21 00:40:40 -0500

    Agregando Listas
    Se agrega ArrayListAgenda, ListAgenda

---
+++ Contacto.java
@@ -0,0 +1,9 @@
+public abstract class Contacto {
+
+
+    private String nombre;
+    private String numeroTelefono;
+
+    
+    
+}
---
+++ ListAgenda.java
@@ -0,0 +1,21 @@
+public interface ListAgenda<E> {
+
+    public void add(E element);
+
+    public void addFirst(E element);
+
+    public E removeLast();
+
+    public E removeFirst();
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public boolean contains(E element);
+
+    public E getFirst();
+
+    public E getLast();
+    
+} 
